79ff44e83e5c080e8c1ac1cd42cd96b522bba58e
junkaiii <junkaiii@gmail.com>
2023-06-06 22:45:38 +0300

    compress video further

--- dist/app.js
+++ dist/app.js
@@ -5,11 +5,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const fetch_1 = require("./fetch");
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+const corsOptions = {
+    origin: "https://creatorial.ai",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json());
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
+});
+app.post("/watermark", (0, cors_1.default)(corsOptions), async (req, res) => {
+    const sourceUrl = req.body.sourceUrl;
+    if (!!sourceUrl) {
+        res.status(400);
+    }
+    // fetch the file from source
+    try {
+        const result = await (0, fetch_1.fetchVideo)(sourceUrl);
+        const remoteDir = sourceUrl.substring(0, sourceUrl.lastIndexOf("/"));
+        res.status(200).send({ result: `${remoteDir}/${result}` });
+    }
+    catch (e) {
+        console.log("something went wrong");
+    }
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- ffmpeg.ts
+++ ffmpeg.ts
@@ -18,13 +18,12 @@ export const compress = async (fileName: string) => {
       .input(`${fileName}`)
       .input("creatorial.png")
       .complexFilter([
-        "[1]format=rgba,colorchannelmixer=aa=0.5[logo]",
-        {
-          inputs: ["0", "logo"],
-          filter: "overlay",
-          options: { x: "(W-w)/2", y: "(H-h)/2" },
-        },
+        "[1]format=rgba,colorchannelmixer=aa=0.3[logo]",
+        "[logo][0]scale2ref=w=iw:h=ow/mdar[logo][video]",
+        "[video][logo]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2[video]",
+        "[video]scale=720:trunc(ow/a/2)*2",
       ])
+      .videoCodec("libx264")
       .output(compressedFileName)
       .on("end", () => {
         console.log("[ffmpeg] finished");
